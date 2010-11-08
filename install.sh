94df4e8fc87e1996cba6514340befd6f4b01a904
zeus <zeus@zeus-desktop.(none)>
2010-11-08 11:58:39 +0300

    modifying install script and bin/ide so they can be used to install and instantiate a screen session

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+dir=`dirname $0`
+dir=`readlink -f $dir`
 rm -f ~/.screenrc
 rm -f ~/.ruby_ide.screenrc
-ln -s `pwd`/screenrc ~/.screenrc
-ln -s `pwd`/ruby_ide.screenrc ~/.ruby_ide.screenrc
+echo "$dir"
+ln -s "$dir"/screenrc ~/.screenrc
+ln -s "$dir"/ruby_ide.screenrc ~/.ruby_ide.screenrc
+
+if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+fi
+rm -f ~/bin/ide
+ln -s "$dir"/bin/ide ~/bin/ide
